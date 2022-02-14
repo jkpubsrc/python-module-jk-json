102ff36327904d209c6731c81b4e051ae8ed1c54
Jürgen Knauth <pubsrc@binary-overflow.de>
2022-02-14 23:47:55 +0100

    Examples for pretty-printing

--- examples/demo-pretty-printing.py
+++ examples/demo-pretty-printing.py
@@ -12,19 +12,11 @@ FILE_NAME = "test-relaxed.json"
 jData = jk_json.loadFromFile(FILE_NAME)
 
 print("=" * 160)
-jk_json.prettyPrint(jData, comment=[
-	"-" * 80,
-	"Pretty printed version of the data in file \"{}\"".format(FILE_NAME),
-	"-" * 80,
-])
+jk_json.prettyPrint(jData)
 print("=" * 160)
 
 
-text = jk_json.prettyPrintToStr(jData, comment=[
-	"-" * 80,
-	"Pretty printed version of the data in file \"{}\"".format(FILE_NAME),
-	"-" * 80,
-])
+text = jk_json.prettyPrintToStr(jData)
 
 jData2 = jk_json.loads(text)
 
